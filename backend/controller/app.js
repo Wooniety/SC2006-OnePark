9d4b7a1a2f682c2c7adf98592316b58ca9223060
Wooniety <wooniety@gmail.com>
2023-10-24 22:44:52 +0800

    package updates

--- backend/controller/app.js
+++ backend/controller/app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 const fs = require('fs');
 const cors = require('cors');
-const validator = require('validator');
 
 const users = require('../model/users.js');
 
@@ -17,164 +16,171 @@ app.use(cors());
 
 // User login
 app.post('/login/', (request, response, next) => {
-  var username = request.body.username;
-  var password = request.body.password;
+	var email = request.body.email;
+	var password = request.body.password;
 
-  users.verify(username, password, (err, result) => {
-    // Error returned
-    if (err) {
-      console.log(err);
-      response.status(400).send("Error occurred");
-      return;
-    }
+	users.verify(email, password, (err, result) => {
+		// Error returned
+		if (err) {
+			console.log(err);
+			response.status(400).send("Error occurred");
+			return;
+		}
 
-    // No error returned
-    else {
-      // No user found / incorrect password
-      if (result === null) {
-        response.status(401).send("Please check your provided details");
-        return;
-      }
+		// No error returned
+		else {
+			// No user found / incorrect password
+			if (result === null) {
+				response.status(401).send("Please check your provided details");
+				return;
+			}
 
-      else {
-        const payload = {
-          user_id: result.USERS_ID
-        }
+			else {
+				const payload = {
+					user_id: result.USERS_ID
+				}
 
-        jwt.sign(payload, JWT_SECRET, { algorithm: 'HS256' }, (err, token) => {
-          // Error occurred
-          if (err) {
-            console.log(err);
-            response.status(400).send("Error occurred");
-            return;
-          }
+				jwt.sign(payload, JWT_SECRET, { algorithm: 'HS256' }, (err, token) => {
+					// Error occurred
+					if (err) {
+						console.log(err);
+						response.status(400).send("Error occurred");
+						return;
+					}
 
-          // NO error occurred
-          else if (!err) {
-            // Remove the 'password' attribute from the user details
-            delete result['PASSWORD'];
+					// NO error occurred
+					else if (!err) {
+						// Remove the 'password' attribute from the user details
+						delete result['PASSWORD'];
 
-            // Return a response
-            response.status(200).send({
-              // For the benefit of the front-end login page
-              success: true,
-              userData: JSON.stringify(result),
-              status: "Successful login",
+						// Return a response
+						response.status(200).send({
+							// For the benefit of the front-end login page
+							success: true,
+							userData: JSON.stringify(result),
+							status: "Successful login",
 
-              user_id: result.USERS_ID
-            });
-            
-            return;
-          };
-        }); // End of jwt.sign()
-      };
-    };
+							user_id: result.USERS_ID
+						});
+						
+						return;
+					};
+				}); // End of jwt.sign()
+			};
+		};
 
-  });
+	});
 });
 
 // Get all users in the database
 app.get('/users/', (request, response, next) => { 
-  users.findAll((err, results) => {
-    // Errors
-    if (err) {
-      console.log(err);
-      response.status(500).send("Internal Server Error");
-      return;
-    }
+	users.findAll((err, results) => {
+		// Errors
+		if (err) {
+			console.log(err);
+			response.status(500).send("Internal Server Error");
+			return;
+		}
 
-    // No error
-    else {
-      response.status(200).send(results);
-      return;
-    }
-  });
+		// No error
+		else {
+			response.status(200).send(results);
+			return;
+		}
+	});
 }); // End of endpoint 1
 
 // Add a new user to the 'users' table
 app.post('/users/', (request, response, next) => { 
-  const requestBody = request.body;
-  //console.log(requestBody)
+	const requestBody = request.body;
+	//console.log(requestBody)
 
-  users.insert(requestBody, (err, results) => {
-    // Errors
-    if (err) {
-      console.log(err);
-      response.status(500).send("Internal Server Error");
-      return;
-    }
+	users.insert(requestBody, (err, results) => {
+		// Errors
+		if (err) {
+			console.log(err);
+			response.status(500).send("Internal Server Error");
+			return;
+		}
 
-    // No errors
-    else {
-      response.status(201).send(`{\n"userID": ${results}\n}`);
-      return;
-    };
-  });
+		// No errors
+		else {
+			response.status(201).send(`{\n"userID": ${results}\n}`);
+			return;
+		};
+	});
 });
 
 // Retrieve a single user by their userid
 app.get('/users/:userID', (request, response, next) => { 
-  const userID = request.params.userID;
-  
-  users.findByID(userID, (err, results) => {
-    // Errors
-    if (err) {
-      console.log(err);
-      response.status(500).send("Internal Server Error");
-      return;
-    }
+	const userID = request.params.userID;
+	
+	users.findByID(userID, (err, results) => {
+		// Errors
+		if (err) {
+			console.log(err);
+			response.status(500).send("Internal Server Error");
+			return;
+		}
 
-    // No errors
-    else {
-      response.status(200).send(results);
-      return;
-    };
-  });
+		// No errors
+		else {
+			response.status(200).send(results);
+			return;
+		};
+	});
 }); 
 
+// Check if email already exists
+// TODO
+app.get('/verify/email', (request, response, next) => { 
+	const email = request.params.email;
+	
+	users.findByEmail(email, (err, results) => {
+		// Errors
+		if (err) {
+			console.log(err);
+			response.status(500).send(results);
+			return;
+		}
+
+		// No errors
+		else {
+			response.status(200).send(results);
+			return;
+		};
+	});
+}); 
 
 // Update a single user
-app.put('/users/:userID', isLoggedin, (request, response, next) => {
-  const userID = request.params.userID;
-  const requestBody = request.body;
+app.put('/users/:userID', (request, response, next) => {
+	const userID = request.params.userID;
+	const requestBody = request.body;
 
-  // Validation
-  if (!(
-    typeof requestBody.username === 'string' &&
-    typeof requestBody.profile_pic_url === 'string' &&
+	users.edit(userID, requestBody, (err) => {
+		// Errors
+		if (err) {
+			// Duplicate username
+			if (err.code == 'ER_DUP_ENTRY') {
+				console.log(err);
+				response.status(422).send("The email already exists");
+				return;
+			}
 
-    Number.isInteger(userID) &&
-  
-    validator.isAlphanumeric(requestBody.username)
-  )) {
-    response.status(400).send("Invalid details");
-    return;
-  }
+			// Other errors
+			else {
+				console.log(err);
+				response.status(500).send("Internal Server Error");
+				return;
+			};
+		}
 
-  users.edit(userID, requestBody, (err) => {
-    // Errors
-    if (err) {
-      // Duplicate username
-      if (err.code == 'ER_DUP_ENTRY') {
-        console.log(err);
-        response.status(422).send("The new username already exists");
-        return;
-      }
-
-      // Other errors
-      else {
-        console.log(err);
-        response.status(500).send("Internal Server Error");
-        return;
-      };
-    }
-
-    // No errors
-    else {
-      response.status(200).send();
-      return;
-    };
-  });
+		// No errors
+		else {
+			response.status(200).send();
+			return;
+		};
+	});
 }); 
 
 module.exports = app;
