d05d67ecb11eea76f51092c9aef75d7866ea53c8
shlai <shihong30@gmail.com>
2023-10-25 12:09:59 +0800

    Update database.js

--- backend/controller/database.js
+++ backend/controller/database.js
@@ -1,12 +1,18 @@
 var mysql = require('mysql');
 
+const db_host = process.env.DB_HOST;
+const db_port = process.env.DB_PORT;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+const db_name = process.env.DB_NAME
+
 const pool = mysql.createPool({
     connectionLimit : 10, // Number of connections to be kept in the pool. Adjust based on your needs.
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'onepark'
+    host: db_host,
+    port: db_port,
+    user: db_user,
+    password: db_password,
+    database: db_name
 });
 
 
